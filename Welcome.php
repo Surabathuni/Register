74241e59e057931259779000507654c5fc0986ff
Surabathuni Prasad <38185200+Surabathuni@users.noreply.github.com>
2019-04-08 14:55:24 +0530

    Add files via upload

---
+++ Welcome.php
@@ -0,0 +1,21 @@
+<?php include('ConnectWelcome.php'); ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="Styl.css">
+	
+</head>
+
+<body>
+
+	<h1> <?php echo "Welcome ". $result["email"]?> <a href="Logout.php"> Logout </a> </h1>
+
+	
+
+</body>	
+
+</html>
